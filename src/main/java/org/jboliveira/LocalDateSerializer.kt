9f9c517c31a0846c11e0073efc1ff9536a5fd583
João Oliveira <jb-oliveira@github.com>
2023-08-13 14:52:55 -0300

    Create POST method

---
+++ src/main/java/org/jboliveira/LocalDateSerializer.kt
@@ -0,0 +1,21 @@
+package org.jboliveira
+import kotlinx.serialization.*
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import java.time.LocalDate
+
+object LocalDateSerializer : KSerializer<LocalDate> {
+    override val descriptor: SerialDescriptor =
+        PrimitiveSerialDescriptor("LocalDate", PrimitiveKind.STRING)
+
+    override fun serialize(encoder: Encoder, value: LocalDate) {
+        encoder.encodeString(value.toString())
+    }
+
+    override fun deserialize(decoder: Decoder): LocalDate {
+        return LocalDate.parse(decoder.decodeString())
+    }
+}
--- src/main/java/org/jboliveira/PersonResource.kt
+++ src/main/java/org/jboliveira/PersonResource.kt
@@ -1,10 +1,15 @@
 package org.jboliveira
 
 import jakarta.inject.Inject
+import jakarta.validation.Valid
+import jakarta.validation.Validator
 import jakarta.ws.rs.GET
+import jakarta.ws.rs.POST
 import jakarta.ws.rs.Path
 import jakarta.ws.rs.Produces
 import jakarta.ws.rs.core.MediaType
+import jakarta.ws.rs.core.Response
+import java.net.URI
 
 @Path("/pessoas")
 class PersonResource {
@@ -12,10 +17,31 @@ class PersonResource {
     @Inject
     lateinit var repository: PersonRepository
 
+    @Inject
+    lateinit var validator: Validator
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     fun list(): List<PersonDTO> {
         return repository.listAll().map { p -> PersonDTO(p) }
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    fun create(@Valid createPersonRequest: PersonDTO): Response {
+        if (!validator.validate(createPersonRequest).isEmpty() ||
+            repository.findByApelido(createPersonRequest.apelido) != null
+        ) {
+            return Response.status(422).build()
+        }
+        val personEntity = PersonEntity(
+            createPersonRequest.apelido,
+            createPersonRequest.nome,
+            createPersonRequest.nascimento,
+            createPersonRequest.stack
+        )
+        repository.persist(personEntity)
+        return Response.created(URI.create("/pessoas/${personEntity.id}")).build()
     }
 
 }
